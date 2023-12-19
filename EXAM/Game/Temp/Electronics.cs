41d8315a5c4d2bfd840d7ddcbc9a734785726eb9
Ilya Kanishev <105855090+kanishevilya@users.noreply.github.com>
2023-12-19 17:45:01 +0600

    electronics

---
+++ EXAM/Game/Temp/Electronics.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework7_8.Problem_02
+{
+    public class Electronics : Product
+    {
+        public Electronics(string name, float price, int quantity, ProductStatus productStatus, ProductType productType = ProductType.Electronics) : base(name, price, quantity, productStatus, productType)
+        {
+
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine("(Electronics) class");
+            base.Print();
+        }
+    }
+}
