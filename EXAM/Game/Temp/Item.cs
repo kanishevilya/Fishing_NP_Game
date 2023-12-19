b51fb897bbe6db976bb0c79b9920ba37b1b7aee7
Ilya Kanishev <105855090+kanishevilya@users.noreply.github.com>
2023-12-19 17:40:37 +0600

    item

---
+++ EXAM/Game/Temp/Item.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApp
+{
+    public class Item
+    {
+        public string Name { get; private set; }
+        public TypeItem Type { get; private set; }
+
+        public Item(string name, TypeItem type)
+        {
+            Name = name;
+            Type = type;
+        }
+    }
+
+    public enum TypeItem
+    {
+        Clothe,
+        Food,
+        Potions,
+    }
+}
