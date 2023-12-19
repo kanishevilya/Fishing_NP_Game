71ff4e8f6501b399ebcca0ee1c2667937aea2679
Ilya Kanishev <105855090+kanishevilya@users.noreply.github.com>
2023-12-19 17:39:05 +0600

    Employee

---
+++ EXAM/Game/Temp/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using methods;
+
+namespace classwork7_8
+{
+    public class Employee: Human
+    {
+        private double salary;
+
+        public Employee(int id, string frstname, string lstname, double salary) : base(id, frstname, lstname)
+        {
+            this.salary = salary;
+            
+        }
+        public override void DoAction()
+        {
+            Console.WriteLine("Action employee");
+        }
+        public override void Print()
+        {
+            base.Print();
+            Console.WriteLine($"Salary = {salary}");
+        }
+        public override string ToString()
+        {
+            return $"Salary = {salary}";
+        }
+    }
+}
