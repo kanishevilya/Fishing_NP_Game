fbdeffd6da92b85bc73afc40d527d77cc463a76d
Ilya Kanishev <105855090+kanishevilya@users.noreply.github.com>
2023-12-19 17:37:50 +0600

    Manager Interface

---
+++ EXAM/Game/Temp/IManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classWork_9_10
+{
+    public interface IManager
+    {
+        List<IWorker> Workers { get; }
+        void Organize();
+        void MakeBudget();
+        void Control();
+    }
+}
