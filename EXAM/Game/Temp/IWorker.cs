9b57b1ee787df37e2ef4b3bf5f7e79cfec05f9f9
Ilya Kanishev <105855090+kanishevilya@users.noreply.github.com>
2023-12-19 17:38:12 +0600

    Worker Interface

---
+++ EXAM/Game/Temp/IWorker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classWork_9_10
+{
+    public interface IWorker : IHuman
+    {
+        event EventHandler WorkEnded;
+
+        bool IsWorkin { get; }
+
+        string Work();
+
+        public void goToHome(bool isNight);
+    }
+    public interface IHuman
+    {
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public DateTime BirthDay { get; set; }
+
+        public void goToHome();
+    }
+}
