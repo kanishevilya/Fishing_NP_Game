703bbea300851b92892df4523302d693053412d0
Ilya Kanishev <105855090+kanishevilya@users.noreply.github.com>
2023-12-19 17:39:29 +0600

    Tutor

---
+++ EXAM/Game/Temp/Tutor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classwork7_8
+{
+    public sealed class Tutor : Employee
+    {
+        public Tutor(int id, string frstname, string lstname, double salary) : base(id, frstname, lstname, salary)
+        {
+
+        }
+        public override void Print()
+        {
+            Console.WriteLine("I'm Tutor!");
+        }
+    }
+}
