99b22be48dc4fd77d9450cb43098a54a79f9964d
Ilya Kanishev <105855090+kanishevilya@users.noreply.github.com>
2023-12-19 17:38:45 +0600

    Curator Class

---
+++ EXAM/Game/Temp/Curator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classwork7_8
+{
+    public class Curator : Employee
+    {
+        public Curator(int id, string frstname, string lstname, double salary) : base(id, frstname, lstname, salary)
+        {
+
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine("I'm Cutor!");
+        }
+    }
+}
