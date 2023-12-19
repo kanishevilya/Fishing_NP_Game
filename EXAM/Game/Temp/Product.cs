e10db40d98dd9aa28fef02fbba468704704e393b
Ilya Kanishev <105855090+kanishevilya@users.noreply.github.com>
2023-12-19 17:44:41 +0600

    product

---
+++ EXAM/Game/Temp/Product.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework7_8.Problem_02
+{
+    public abstract class Product
+    {
+
+        protected string name;
+        protected float price;
+        protected int quantity;
+
+        protected ProductType productType;
+        public ProductStatus ProductStatus { get; set; }
+        public Product(string name, float price, int quantity, ProductStatus productStatus, ProductType productType)
+        {
+            this.name = name;
+            this.price = price;
+            this.quantity = quantity;
+            this.ProductStatus = productStatus;
+            this.productType = productType;
+        }
+
+        public virtual void Print()
+        {
+            Console.WriteLine($"Name          = {name}");
+            Console.WriteLine($"Price         = {price}");
+            Console.WriteLine($"Quantity      = {quantity}");
+            Console.WriteLine($"ProductType   = {productType}");
+            //Console.WriteLine($"ProductStatus = {ProductStatus.GetType().Name}");
+        }
+    }
+    public enum ProductType
+    {
+        Foodstuffs,
+        HouseholdChemicals,
+        Furniture,
+        Electronics
+    }
+    public enum ProductStatus
+    {
+        Came, Implemented, WrittenOff, Transferred
+    }
+}
