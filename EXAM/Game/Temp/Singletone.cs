259d79fc8c2172dad139c9b89d3632f1a901bf90
Ilya Kanishev <105855090+kanishevilya@users.noreply.github.com>
2023-12-19 17:41:10 +0600

    Singletone

---
+++ EXAM/Game/Temp/Singletone.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApp
+{
+    public class Singletone<T>
+    {
+        private static bool onInit;
+        
+        public static T Instance;
+
+        
+        //public bool InInit { get; private set; }
+        public static void Initialize(T instance) {
+            if (!onInit)
+            {
+                onInit = true;
+                Instance = instance;
+
+                //AfterInitialize();
+            }
+        }
+        //protected virtual static void AfterInitialize() { }
+
+    }
+}
