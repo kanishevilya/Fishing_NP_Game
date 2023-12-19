74f584248a2ad134fb20177f2480e92fea37cfcd
Ilya Kanishev <105855090+kanishevilya@users.noreply.github.com>
2023-12-19 17:40:21 +0600

    player

---
+++ EXAM/Game/Temp/Player.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApp
+{
+    public class Player : Character
+    {
+        public Player(string name, int age, List<Item> items) : base(name, age, items) { }
+    }
+}
